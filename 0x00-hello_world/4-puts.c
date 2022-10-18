80a7b1123d2ab79db38c5c215e01357e9a4cb9c6
cryptomlj <melvinokpalaeze@gmail.com>
2022-10-17 17:14:16 -0700

    solutions to the task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - This is the entry point of the program
+ * printing with puts and not printf
+ * Return: 0 (sucessful)
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
---
+++ 0x00-hello_world/README.md
@@ -0,0 +1,2 @@
+This file contains the solutions to Introduction to C project
+Task 0 : the script that runs a C file through the preprocessor and save the result into another file($CFILE).
