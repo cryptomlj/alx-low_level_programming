bbf7000095dc7048c0035492d76c748455a87577
cryptomlj <melvinokpalaeze@gmail.com>
2022-11-24 14:24:39 -0800

    Singly list task

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,37 +1,22 @@
 #include "lists.h"
 
 /**
- * print_list - print elements in a linked list
- *
- * @h: pointer to head first node
- *
- * Return: Number of nodes
-*/
-
+ * print_list - prints all the elements of a list_t list
+ * @h: head of linked list
+ * Return: number of elements
+ */
 size_t print_list(const list_t *h)
 {
-	size_t node_count = 1;
+	size_t size;
 
-	/*return 0 as number of nodes*/
-	if (h == NULL)
-		return (0);
-
-	while (h->next != NULL)
+	size = 0;
+	for (size = 0; h != NULL; size++)
 	{
-		/*if str is NULL return len as 0 and str as (nil)*/
 		if (h->str == NULL)
-			printf("[%d] %s\n", 0, "(nil)");
+			printf("[%d] (nil)\n", 0);
 		else
 			printf("[%d] %s\n", h->len, h->str);
-
-		/*go to the next node*/
 		h = h->next;
-		/*count number of nodes*/
-		node_count += 1;
 	}
-
-	/*print for the last node*/
-	printf("[%d] %s\n", h->len, h->str);
-
-	return (node_count);
+	return (size);
 }
--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,29 +1,17 @@
 #include "lists.h"
 
 /**
- * list_len - gets the number of nodes
- *
- * @h: pointer to the head (first node)
- *
- * Return: number of nodes
-*/
-
+ * list_len - returns the number of elements in a linked list_t list
+ * @h: head of linked list
+ * Return: numberof elements
+ */
 size_t list_len(const list_t *h)
 {
-	size_t node_count = 1;
+	size_t size;
 
-	/*if head is null return 0 nodes*/
-	if (h == NULL)
-		return (0);
-
-	/*iterate through the nodes*/
-	while (h->next != NULL)
-	{
-		/*go to the next node*/
+	for (size = 0; h != NULL; size++)
 		h = h->next;
-		/*count node*/
-		node_count++;
-	}
 
-	return (node_count);
+	return (size);
 }
+
